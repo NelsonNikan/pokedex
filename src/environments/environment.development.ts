110e9879613472f2a16d57e4de0d79c34ecfca0c
NelsonNikan <44512976+NelsonNikan@users.noreply.github.com>
2023-10-22 00:26:20 -0300

    Git Pages

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'pokedex-9dae4',
-    appId: '1:738158047621:web:27d3fb57985dcb04fd94ec',
-    storageBucket: 'pokedex-9dae4.appspot.com',
-    apiKey: 'AIzaSyBofMyro1LyoJv7-mEnZzYxjMFFhYRM7yM',
-    authDomain: 'pokedex-9dae4.firebaseapp.com',
-    messagingSenderId: '738158047621',
-    measurementId: 'G-385V5PCL2Q',
   },};
